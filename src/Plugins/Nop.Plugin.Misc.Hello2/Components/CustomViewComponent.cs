bee9a69a4ebcff8d77be59269e0c5a51a2eb0c5f
Belekz <pieter.dewitte@hotmail.be>
2021-03-05 19:49:49 +0100

    first hello world widget

---
+++ src/Plugins/Nop.Plugin.Misc.Hello2/Components/CustomViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Components;
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Misc.Hello2.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class ExampleWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone)
+        {
+            return Content("Hello World");
+        }
+    }
+
+}
