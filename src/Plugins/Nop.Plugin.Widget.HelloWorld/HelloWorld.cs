4d05e4618d17fabe0e4f4032d7474a05444461cc
Belekz <pieter.dewitte@hotmail.be>
2021-03-05 19:50:12 +0100

    try of hello world

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widget.HelloWorld
+{
+    public class HelloWorld : BasePlugin
+    {
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+    }
+}
