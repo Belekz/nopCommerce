6e0af361c5f037f5beab2a3c0af928a96fbba910
Belekz <pieter.dewitte@hotmail.be>
2021-03-07 14:19:10 +0100

    build successful

---
+++ src/Plugins/Nop.Plugin.Payments.ManualMollie/Controllers/PaymentManualMollieController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Plugin.Payments.ManualMollie.Models;
+using Nop.Services;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Messages;
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.Payments.ManualMollie.Controllers
+{
+    [AuthorizeAdmin]
+    [Area(AreaNames.Admin)]
+    [AutoValidateAntiforgeryToken]
+    public class PaymentManualMollieController : BasePaymentController
+    {
+        #region Fields
+        
+        private readonly ILocalizationService _localizationService;
+        private readonly INotificationService _notificationService;
+        private readonly IPermissionService _permissionService;
+        private readonly ISettingService _settingService;
+        private readonly IStoreContext _storeContext;
+
+        #endregion
+
+        #region Ctor
+
+        public PaymentManualMollieController(ILocalizationService localizationService,
+            INotificationService notificationService,
+            IPermissionService permissionService,
+            ISettingService settingService,
+            IStoreContext storeContext)
+        {
+            _localizationService = localizationService;
+            _notificationService = notificationService;
+            _permissionService = permissionService;
+            _settingService = settingService;
+            _storeContext = storeContext;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IActionResult Configure()
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManagePaymentMethods))
+                return AccessDeniedView();
+
+            //load settings for a chosen store scope
+            var storeScope = _storeContext.ActiveStoreScopeConfiguration;
+            var manualPaymentSettings = _settingService.LoadSetting<ManualMolliePaymentSettings>(storeScope);
+
+            var model = new ConfigurationModel
+            {
+                TransactModeId = Convert.ToInt32(manualPaymentSettings.TransactMode),
+                AdditionalFee = manualPaymentSettings.AdditionalFee,
+                AdditionalFeePercentage = manualPaymentSettings.AdditionalFeePercentage,
+                TransactModeValues = manualPaymentSettings.TransactMode.ToSelectList(),
+                ActiveStoreScopeConfiguration = storeScope
+            };
+            if (storeScope > 0)
+            {
+                model.TransactModeId_OverrideForStore = _settingService.SettingExists(manualPaymentSettings, x => x.TransactMode, storeScope);
+                model.AdditionalFee_OverrideForStore = _settingService.SettingExists(manualPaymentSettings, x => x.AdditionalFee, storeScope);
+                model.AdditionalFeePercentage_OverrideForStore = _settingService.SettingExists(manualPaymentSettings, x => x.AdditionalFeePercentage, storeScope);
+            }
+
+            return View("~/Plugins/Payments.ManualMollie/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        public IActionResult Configure(ConfigurationModel model)
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManagePaymentMethods))
+                return AccessDeniedView();
+
+            if (!ModelState.IsValid)
+                return Configure();
+
+            //load settings for a chosen store scope
+            var storeScope = _storeContext.ActiveStoreScopeConfiguration;
+            var manualMolliePaymentSettings = _settingService.LoadSetting<ManualMolliePaymentSettings>(storeScope);
+
+            //save settings
+            manualMolliePaymentSettings.TransactMode = (TransactMode)model.TransactModeId;
+            manualMolliePaymentSettings.AdditionalFee = model.AdditionalFee;
+            manualMolliePaymentSettings.AdditionalFeePercentage = model.AdditionalFeePercentage;
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared 
+             * and loaded from database after each update */
+
+            _settingService.SaveSettingOverridablePerStore(manualMolliePaymentSettings, x => x.TransactMode, model.TransactModeId_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(manualMolliePaymentSettings, x => x.AdditionalFee, model.AdditionalFee_OverrideForStore, storeScope, false);
+            _settingService.SaveSettingOverridablePerStore(manualMolliePaymentSettings, x => x.AdditionalFeePercentage, model.AdditionalFeePercentage_OverrideForStore, storeScope, false);
+            
+            //now clear settings cache
+            _settingService.ClearCache();
+
+            _notificationService.SuccessNotification(_localizationService.GetResource("Admin.Plugins.Saved"));
+
+            return Configure();
+        }
+
+        #endregion
+    }
+}
