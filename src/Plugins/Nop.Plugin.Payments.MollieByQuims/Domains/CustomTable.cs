42b20900118af90fe4f04f7c05ae57f1f5cb8848
Belekz <pieter.dewitte@hotmail.be>
2021-04-17 15:32:35 +0200

    remove mollie by quims

--- src/Plugins/Nop.Plugin.Payments.MollieByQuims/Domains/CustomTable.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Core;
-
-namespace Nop.Plugin.Payments.MollieByQuims.Domains
-{
-    public partial class CustomTable : BaseEntity
-    {
-
-    }
-}
--- src/Plugins/Nop.Plugin.Payments.MollieByQuims/Services/Payment/IPaymentStorageClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using Nop.Plugin.Payments.MollieByQuims.Models;
-
-namespace Nop.Plugin.Payments.MollieByQuims.Services.Payment
-{
-    public interface IPaymentStorageClient {
-        Task Create(CreatePaymentModel model);
-    }
-}
