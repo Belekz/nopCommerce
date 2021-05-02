b13dd550f7f411f28c5f5bf9ffa668573184f162
Belekz <pieter.dewitte@hotmail.be>
2021-05-02 11:38:31 +0200

    side cart base functions working

--- src/Plugins/Nop.Plugin.Widgets.SideShoppingCart/Components/CustomViewComponent.cs
+++ src/Plugins/Nop.Plugin.Widgets.SideShoppingCart/Components/CustomViewComponent.cs
@@ -7,6 +7,9 @@ using Nop.Web.Framework.Components;
 using Nop.Plugin.Widgets.SideShoppingCart.Models;
 using System;
 using System.Threading.Tasks;
+using Nop.Services.Security;
+using Nop.Web.Factories;
+using Nop.Core.Domain.Orders;
 
 namespace Nop.Plugin.Widgets.SideShoppingCart.Components
 {
@@ -18,26 +21,34 @@ namespace Nop.Plugin.Widgets.SideShoppingCart.Components
         private readonly ISettingService _settingService;
         private readonly IPictureService _pictureService;
         private readonly IWebHelper _webHelper;
+        private readonly IPermissionService _permissionService;
+        private readonly IShoppingCartModelFactory _shoppingCartModelFactory;
+        private readonly ShoppingCartSettings _shoppingCartSettings;
 
-        public CustomViewComponent(IStoreContext storeContext, IStaticCacheManager staticCacheManager, 
-            ISettingService settingService, IPictureService pictureService, IWebHelper webHelper)
+        public CustomViewComponent(IStoreContext storeContext, 
+            IStaticCacheManager staticCacheManager, 
+            ISettingService settingService, 
+            IPictureService pictureService, IWebHelper webHelper,
+            IPermissionService permissionService,
+            IShoppingCartModelFactory shoppingCartModelFactory,
+            ShoppingCartSettings shoppingCartSettings)
         {
             _storeContext = storeContext;
             _staticCacheManager = staticCacheManager;
             _settingService = settingService;
             _pictureService = pictureService;
             _webHelper = webHelper;
+            _permissionService = permissionService;
+            _shoppingCartModelFactory = shoppingCartModelFactory;
+            _shoppingCartSettings = shoppingCartSettings;
         }
 
         /// <returns>A task that represents the asynchronous operation</returns>
         public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
         {
-            var nivoSliderSettings = await _settingService.LoadSettingAsync<SideShoppingCartSettings>((await _storeContext.GetCurrentStoreAsync()).Id);
+            var sideShoppingCart = await _settingService.LoadSettingAsync<SideShoppingCartSettings>((await _storeContext.GetCurrentStoreAsync()).Id);
 
-            var model = new PublicInfoModel
-            {
-                Item = "test item 1"
-            };
+            var model = await _shoppingCartModelFactory.PrepareMiniShoppingCartModelAsync();
 
             return View("~/Plugins/Widgets.SideShoppingCart/Views/PublicInfo.cshtml", model);
         }
